d2a6b08c7aaebe258aa2e011f2206092b6fa0ab8
Nate Dunn <hello@natedunn.net>
2019-11-14 11:09:03 -0600

    Add header, video, and sponsors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@ module.exports = {
   prefix: '',
   important: false,
   separator: ':',
-
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        mint: '#47c386',
+        'astro-blue': '#1b465b',
+        'summary-sky': '#36b2db',
+        'dove-gray': '#6f6f6c',
+      },
+    },
+  },
   variants: {},
   corePlugins: {},
   plugins: [],
